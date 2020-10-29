56de0ca1aedfba5bebb652db4ea351ba6923b602
R4falek <rafal888@onet.pl>
2020-10-29 09:53:54 +0100

    Update

--- Calculator.py
+++ Calculator.py
@@ -5,7 +5,7 @@ operators = {"+", "-", "*", "/"}
 numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
 
 
-def is_valid(list_of_formulas): # sprawdza czy użytkownik nie podał błędnych znaków
+def is_valid(list_of_formulas):  # sprawdza czy użytkownik nie podał błędnych znaków
     legal_sings = operators.union(numbers)
     for word in list_of_formulas:
         for letter in word:
@@ -14,7 +14,7 @@ def is_valid(list_of_formulas): # sprawdza czy użytkownik nie podał błędnych
     return True
 
 
-def split_sentence(sentence): # rozdziela dane na znaki i liczby tz 1+22 => 1, +, 22
+def split_sentence(sentence):    # rozdziela dane na znaki i liczby tz 1+22 => 1, +, 22
     formulas = []
     index = 0
     last_sing_index = -1
@@ -44,13 +44,13 @@ def remove_unwanted_sings(list_of_formulas):
         list_of_formulas.insert(0, "0")
 
     # removing sings from end
-    while list_of_formulas[-1] in operators: # usuwa niepotrzebne znaki z końca
+    while list_of_formulas[-1] in operators:     # usuwa niepotrzebne znaki z końca
         list_of_formulas.pop()
 
     return list_of_formulas
 
 
-def simple_calc(a, operator, b): # potrafi wykonać bazowe obliczenia jak 1+3
+def simple_calc(a, operator, b):     # potrafi wykonać bazowe obliczenia jak 1+3
     a = int(a)
     b = int(b)
     if operator == "+":
@@ -65,7 +65,7 @@ def simple_calc(a, operator, b): # potrafi wykonać bazowe obliczenia jak 1+3
     return 0
 
 
-def priority_calc(operators_set, formulas): # wykonuje działania na operatorach danych w operators_set
+def priority_calc(operators_set, formulas):  # wykonuje działania na operatorach danych w operators_set
     sth_hanged = False
     i = 0
     while i < len(formulas):
@@ -93,7 +93,7 @@ class Calculator:
         wk = 2    # wysokosc przyciskow
 
         self.entry = Entry(self.master)
-        self.entry.grid(row=0, column=0, columnspan=3,sticky=N+S+W+E)
+        self.entry.grid(row=0, column=0, columnspan=3, sticky=N+S+W+E)
 
         self.calculate = Button(self.master, text="=", height=wk, width=sk, command=self.calculate)
         self.calculate.grid(row=0, column=3, rowspan=2)
@@ -104,46 +104,46 @@ class Calculator:
         self.quitButton = Button(self.master, text='X', command=self.close_windows, bg="red", width=sk, height=wk)
         self.quitButton.grid(row=0, column=4, rowspan=2, sticky=N+S)
 
-        self.nineButton = Button(self.master, text='9', height=wk, width=sk, command=lambda: self.click(9))
+        self.nineButton = Button(self.master, text='9', height=wk, width=sk, command=lambda: self.click('9'))
         self.nineButton.grid(row=2, column=0)
 
-        self.eightButton = Button(self.master, text='8', height=wk, width=sk, command=lambda: self.click(8))
+        self.eightButton = Button(self.master, text='8', height=wk, width=sk, command=lambda: self.click('8'))
         self.eightButton.grid(row=2, column=1)
 
-        self.sevenButton = Button(self.master, text='7', height=wk, width=sk, command=lambda: self.click(7))
+        self.sevenButton = Button(self.master, text='7', height=wk, width=sk, command=lambda: self.click('7'))
         self.sevenButton.grid(row=2, column=2)
 
         self.divisionButton = Button(self.master, text='/', height=wk, width=sk, command=lambda: self.click("/"))
         self.divisionButton.grid(row=2, column=3)
 
-        self.sixButton = Button(self.master, text='6', height=wk, width=sk, command=lambda: self.click(6))
+        self.sixButton = Button(self.master, text='6', height=wk, width=sk, command=lambda: self.click('6'))
         self.sixButton.grid(row=3, column=0)
 
-        self.fiveButton = Button(self.master, text='5', height=wk, width=sk, command=lambda: self.click(5))
+        self.fiveButton = Button(self.master, text='5', height=wk, width=sk, command=lambda: self.click('5'))
         self.fiveButton.grid(row=3, column=1)
 
-        self.fourButton = Button(self.master, text='4', height=wk, width=sk, command=lambda: self.click(4))
+        self.fourButton = Button(self.master, text='4', height=wk, width=sk, command=lambda: self.click('4'))
         self.fourButton.grid(row=3, column=2)
 
         self.multiplicationButton = Button(self.master, text='*', height=wk, width=sk, command=lambda: self.click("*"))
         self.multiplicationButton.grid(row=3, column=3)
 
-        self.treeButton = Button(self.master, text='3', height=wk, width=sk, command=lambda: self.click(3))
+        self.treeButton = Button(self.master, text='3', height=wk, width=sk, command=lambda: self.click('3'))
         self.treeButton.grid(row=4, column=0)
 
-        self.twoButton = Button(self.master, text='2', height=wk, width=sk, command=lambda: self.click(2))
+        self.twoButton = Button(self.master, text='2', height=wk, width=sk, command=lambda: self.click('2'))
         self.twoButton.grid(row=4, column=1)
 
-        self.oneButton = Button(self.master, text='1', height=wk, width=sk, command=lambda: self.click(1))
+        self.oneButton = Button(self.master, text='1', height=wk, width=sk, command=lambda: self.click('1'))
         self.oneButton.grid(row=4, column=2)
 
         self.subtractionButton = Button(self.master, text='-', height=wk, width=sk, command=lambda: self.click("-"))
         self.subtractionButton.grid(row=4, column=3)
 
-        self.clearButton = Button(self.master, text='C', height=wk, width=sk, command= self.clear)
+        self.clearButton = Button(self.master, text='C', height=wk, width=sk, command=self.clear)
         self.clearButton.grid(row=5, column=0)
 
-        self.zeroButton = Button(self.master, text='0', height=wk, width=sk, command=lambda: self.click(0))
+        self.zeroButton = Button(self.master, text='0', height=wk, width=sk, command=lambda: self.click('0'))
         self.zeroButton.grid(row=5, column=1)
 
         self.dotButton = Button(self.master, text='.', height=wk, width=sk, command=lambda: self.click("."))
@@ -154,7 +154,6 @@ class Calculator:
 
         self.result.delete(0, "end")
 
-
     def clear(self):             # komenda czyszczenia
         self.entry.delete(0, 'end')
         self.result.delete(0, 'end')
@@ -162,9 +161,14 @@ class Calculator:
     def close_windows(self):    # komenda zamykania
         self.master.destroy()
 
-    def click(self, nmbr, i=100): # wypisywanie cyfr po kliknieciu przycisku
+    def click(self, nmbr):  # wypisywanie cyfr po kliknieciu przycisku
+        if self.entry.get() == '0':
+            self.entry.delete(0, 'end')
+
         self.result.delete(0, 'end')
-        self.entry.insert(i, nmbr)
+        current = self.entry.get()
+        self.entry.delete(0, 'end')
+        self.entry.insert(0, str(current) + str(nmbr))
 
     def calculate(self):
         self.result.delete(0, 'end')
@@ -189,5 +193,3 @@ class Calculator:
         result = formulas[0]
 
         self.result.insert(0, result)
-
-
