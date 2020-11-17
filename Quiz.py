92de7966f0d94103b77698d2b6aa88964f2ebbe3
Lukasz Suchan <lukasz.suchan@interia.eu>
2020-11-17 12:28:47 +0100

    Wstęp do Statystyki
    na końcu wyświrtla się wynik i przycisk do ponownej gry

--- Quiz.py
+++ Quiz.py
@@ -14,7 +14,7 @@ class Quiz:
         self.master.iconbitmap(r'Images\earth_icon.ico')
         self.frame = Frame(self.master)
         self.frame.pack()
-        self.quitButton = Button(self.frame, text = 'X', padx=15,pady=10, bg = "red",command=self.close_windows)
+        #self.quitButton = Button(self.frame, text = 'X', padx=15,pady=10, bg = "red",command=self.close_windows)
         #self.quitButton.grid(row =0, column=2)
         self.startLabel = Label(self.frame, text="Aby rozpocząć kliknij przycisk Start")
         self.startLabel.grid(row=1,column=0)
@@ -27,40 +27,44 @@ class Quiz:
 
         self.countryList = CountryIMG.CountryIMG()
         self.numberOfQuestion = 10
-        self.QuizList = random.sample(self.countryList.Country,self.numberOfQuestion)
+
 
     def Start(self):
-        self.startButton.destroy()
-        self.startLabel.destroy()
-        self.startImageLabel.destroy()
+        self.frame.destroy()
+        #self.startButton.destroy()
+        #self.startLabel.destroy()
+        #self.startImageLabel.destroy()
         self.master.geometry("500x600")
         self.Quiz_work()
 
     def Quiz_work(self):
+        self.QuizList = random.sample(self.countryList.Country, self.numberOfQuestion)
+        self.QuizFrame = Frame(self.master)
+        self.QuizFrame.pack()
         self.QuestionNumber = -1
         self.points = 0
 
         self.tmpName=self.QuizList[0][1]
 
-        self.Question = Label(self.frame)
+        self.Question = Label(self.QuizFrame)
         self.Question.grid(row=0, column=0, columnspan=5)
 
-        self.countryLabel = Label(self.frame)
+        self.countryLabel = Label(self.QuizFrame)
         self.countryLabel.grid(row=1, column=0, columnspan=5)
         #Country name do celów debugerskich quizu
-        self.countryName = Label(self.frame)
+        self.countryName = Label(self.QuizFrame)
         self.countryName.grid(row=2, column=0, columnspan=5)
 
-        self.odpA = Button(self.frame, command=lambda:self.Next_Question(self.odpA.cget('text')))
+        self.odpA = Button(self.QuizFrame, command=lambda:self.Next_Question(self.odpA.cget('text')))
         self.odpA.grid(row=3, column=0,)
-        self.odpB = Button(self.frame, command=lambda:self.Next_Question(self.odpB.cget('text')))
+        self.odpB = Button(self.QuizFrame, command=lambda:self.Next_Question(self.odpB.cget('text')))
         self.odpB.grid(row=3, column=1)
-        self.odpC = Button(self.frame, command=lambda:self.Next_Question(self.odpC.cget('text')))
+        self.odpC = Button(self.QuizFrame, command=lambda:self.Next_Question(self.odpC.cget('text')))
         self.odpC.grid(row=3, column=2)
-        self.odpD = Button(self.frame , command =lambda:self.Next_Question(self.odpD.cget("text")))
+        self.odpD = Button(self.QuizFrame , command =lambda:self.Next_Question(self.odpD.cget("text")))
         self.odpD.grid(row =3,column =4)
 
-        self.Wynik = Label(self.frame)
+        self.Wynik = Label(self.QuizFrame)
         self.Wynik.grid(row=4, column=0, columnspan=5,pady=50)
 
         self.Next_Question()
@@ -90,9 +94,20 @@ class Quiz:
 
             self.Wynik.config(text="wynik: "+str(self.points)+"/"+str(self.numberOfQuestion))
         else:
-            self.close_windows()
-            #wyświetlenie ekranu końcowego
+            self.stat_window()
 
+    def stat_window(self):
+        self.QuizFrame.destroy()
+        self.Statframe = Frame(self.master)
+        self.Statframe.pack()
+        self.StatystykaWynik = Label(self.Statframe,text="wynik: "+str(self.points)+"/"+str(self.numberOfQuestion))
+        self.StatystykaWynik.grid(row=0,column=0,padx=50, pady=50)
+
+        self.ReplayButton = Button(self.Statframe,text="Spróbuj jeszcze raz",command =self.Replay)
+        self.ReplayButton.grid(row=1,pady=20)
+    def Replay(self):
+        self.Statframe.destroy()
+        self.Quiz_work()
 
     def close_windows(self):
         self.master.destroy()
