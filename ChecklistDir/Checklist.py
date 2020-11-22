77406b39e52b4909e1e38543cc0043c68d8bcc56
Bartosz Sambór <72545684+BartoszSambor@users.noreply.github.com>
2020-11-22 12:22:57 +0100

    Update Checklist.py

--- ChecklistDir/Checklist.py
+++ ChecklistDir/Checklist.py
@@ -3,33 +3,30 @@ from ChecklistDir import CreateButton
 from ChecklistDir import DropDown
 
 
-
 class Checklist:
 
     def __init__(self, master, parent):
         self.master = master
         self.master.title("Checklisty")
         self.master.geometry("500x500")
-        #self.frame = Frame(self.master)
-        #self.frame.pack()
-        #self.parent = parent
+        # self.frame = Frame(self.master)
+        # self.frame.pack()
+        # self.parent = parent
 
-
-
-        #deklaracja przycisków
+        # deklaracja przycisków
         self.MakeChecklist = Button(self.master, text="Utworz nowa Checkliste", padx=30, pady=50, command=self.new_window1)
         self.DropDown = Button(self.master, text="Tu będzie wybor", padx=10, pady=50, command=self.new_window2)
         self.quitButton = Button(self.master, text='Wyjście', padx=80, pady= 10,  command=self.close_windows)
 
-        #ustawienie w oknie
+        # ustawienie w oknie
         self.DropDown.grid(row=0, column=2,  padx=5, pady=10, columnspan=1, sticky= W )
-        self.MakeChecklist.grid(row=0, column=0, padx=5, pady=10, columnspan=1)
-        self.quitButton.grid(row=1, column=0, padx=10, pady=10)
+        self.MakeChecklist.grid(row=1, column=2, padx=5, pady=10, columnspan=1)
+        self.quitButton.grid(row=0, column=0, padx=10, pady=10)
 
     def close_windows(self):
         self.master.destroy()
 
-    #wyświetlanie w nowym oknie
+    # wyświetlanie w nowym oknie
     def new_window1(self):
         new_window = Toplevel(self.master)
         CreateButton.CreateButton(new_window, self)
